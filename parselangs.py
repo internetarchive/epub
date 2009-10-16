d47cd4cef825ef5564f1cbe80817f90f49d16a16
Mike McCabe <mccabe@ia311018.us.archive.org>
2009-10-16 01:53:58 +0000

    add some language mapping generation madness

---
+++ parselangs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from debug import debug, debugging, assert_d
+
+import json
+
+from lxml import html
+#from lxml.html import soupparser as s
+from lxml import etree
+
+t = html.parse('langs.htm')
+table = t.getroot()[1][0]
+
+result = {}
+
+iso639_2_T = {}
+iso639_2_B = {}
+iso639_3 = {}
+lang_name = {}
+native_name = {}
+
+first = True
+for row in table:
+    if first:
+        first = False
+        continue
+    abbrev = row[0].text
+    iso639_2_T[row[1].text] = abbrev
+    if row[2].text != u'-':
+        iso639_2_B[row[2].text] = abbrev
+    if row[3].text != u'—' and row[3].text != u'-':
+        iso639_3[row[3].text[:3]] = abbrev
+    if len(row[4]) > 0:
+        coltext = etree.tostring(row[4], method='text', encoding=unicode)
+        for name in coltext[:-1].split(', '):
+            lang_name[name] = abbrev
+    if row[5].text is not None:
+        for name in row[5].text.split(', '):
+            native_name[name] = abbrev
+
+print 'iso639_2_T = ' + json.dumps(iso639_2_T, sort_keys=True, indent=4)
+print 'iso639_2_B = ' + json.dumps(iso639_2_B, sort_keys=True, indent=4)
+print 'iso639_3 = ' + json.dumps(iso639_3, sort_keys=True, indent=4)
+print 'lang_name = ' + json.dumps(lang_name, sort_keys=True, indent=4)
+print 'native_name = ' + json.dumps(native_name, sort_keys=True, indent=4)
+
