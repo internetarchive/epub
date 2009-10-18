3e7b9effe8dd58b95d0485dd11e2d6eab247f83d
Mike McCabe <mccabe@archive.org>
2009-10-18 02:50:30 -0700

    Move got_book_id to iarchive.infer_book_id

--- common.py
+++ common.py
@@ -11,15 +11,6 @@ import sys
 
 from lxml import etree
 
-def get_book_id():
-    files=os.listdir(".")
-    #ignore files starting with '.' using list comprehension
-    files=[filename for filename in files if filename[0] != '.']
-    for fname in files:
-        if re.match('.*_abbyy.gz$', fname):
-            return re.sub('_abbyy.gz$', '', fname)
-    print "couldn't get book id"
-    return None
 
 
 def tree_to_str(tree, xml_declaration=True):
