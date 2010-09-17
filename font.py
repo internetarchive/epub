76f4c28b5f9f0172ba719f5a7336cfc4cf648a56
Mike McCabe <mccabe@archive.org>
2010-09-16 23:58:15 -0700

    add publicly available fonts for use by visualize_ocr

--- font.py
+++ font.py
@@ -1,4 +1,5 @@
-font_dir = '/home/mike/s/archive/epub/ttf/'
+import sys
+font_dir = sys.path[0] + '/fonts/'
 font_mapping = { "Times New Roman" : "TimesRoman.ttf",
                  "Georg" : "TimesRoman.ttf",
                  "georg" : "TimesRoman.ttf",
@@ -11,6 +12,8 @@ font_mapping = { "Times New Roman" : "TimesRoman.ttf",
                  "Courier" : "Courier.ttf",
                  "Courier New" : "Courier.ttf"
                  }
+fallback = "Helvetica.ttf"
+
 italic_font_mapping = {
                  "Times New Roman" : "TimesItalic.ttf",
                  "Georg" : "TimesItalic.ttf",
@@ -22,6 +25,42 @@ italic_font_mapping = {
                  "Courier" : "CourierOblique.ttf",
                  "Courier New" : "CourierOblique.ttf"
                  }
+fallback_italic = "HelveticaOblique.ttf"
+
+droid_font_mapping = { "Times New Roman" : "DroidSerif-Regular.ttf",
+                       "Georg" : "DroidSerif-Regular.ttf",
+                       "georg" : "DroidSerif-Regular.ttf",
+                       "Arial" : "DroidSans.ttf",
+                       "Verda" : "DroidSans.ttf",
+                       "verda" : "DroidSans.ttf",
+                       "PalatinoLinotype" : "DroidSans.ttf",
+                       "palatinolinotype" : "DroidSans.ttf",
+                       "Tahoma" : "DroidSans.ttf",
+                       "Courier" : "DroidSansMono.ttf",
+                       "Courier New" : "DroidSansMono.ttf"
+                 }
+droid_fallback = "DroidSerif-Regular.ttf"
+
+droid_italic_font_mapping = {
+                 "Times New Roman" : "DroidSerif-Italic.ttf",
+                 "Georg" : "DroidSerif-Italic.ttf",
+                 "georg" : "DroidSerif-Italic.ttf",
+                 "Arial" : "DroidSans.ttf",
+                 "PalatinoLinotype" : "DroidSans.ttf",
+                 "palatinolinotype" : "DroidSans.ttf",
+                 "Tahoma" : "DroidSans.ttf",
+                 "Courier" : "DroidSansMono.ttf",
+                 "Courier New" : "DroidSansMono.ttf"
+                 }
+droid_fallback_italic = "DroidSerif-Italic.ttf"
+
+if True:
+    font_mapping = droid_font_mapping
+    italic_font_mapping = droid_italic_font_mapping
+    fallback = droid_fallback
+    fallback_italic = droid_fallback_italic
+
+
 fonts = {}
 
 import Image
@@ -41,14 +80,10 @@ def get_font(name, dpi, size, italic=False):
                 mapped = font_mapping[name]
         except KeyError:
             if italic:
-                mapped = "HelveticaOblique.ttf"
+                mapped = fallback_italic
             else:
-                mapped = "Helvetica.ttf"
+                mapped = fallback
         font = ImageFont.truetype(font_dir + mapped, size)
         family[size] = font
     font = family[size]
     return font
-
-
-#         f = ImageFont.load_default()
-# http://www.ampsoft.net/webdesign-l/WindowsMacFonts.html
