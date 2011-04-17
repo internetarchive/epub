f06815f2afaf39d90ab693bac6b3ab19219a1ea5
Kenneth Haase <haase@silverback.local>
2011-04-17 11:42:40 -0700

    Added protoype dual viewer

---
+++ beer/proto.js
@@ -0,0 +1,53 @@
+var img_template=false;
+var pageno=false;
+
+function init()
+{
+    if (img_template) return img_template;
+    var meta_items=document.getElementsByTagName("meta");
+    var i=0; var lim=meta_items.length;
+    while (i<lim) {
+	if (meta_items[i].name==="IMGTEMPLATE") {
+	    img_template=meta_items[i].content;
+	    break;}
+	else i++;}
+    return img_template;
+}
+
+function gotoPage(n)
+{
+    if (!(n)) n=pageno;
+    if (!(n)) n=parseInt(document.body.getAttribute("data-startpage"));
+    var pagestring=n.toString();
+    while (pagestring.length<4) pagestring="0"+pagestring;
+    var img_elt=document.getElementById("PAGEIMAGE");
+    img_elt.src=img_template.replace("%%%%",pagestring);
+    var found=document.getElementsByClassName("displayed");
+    var current=[]
+    var i=0; var lim=found.length;
+    while (i<lim) {current[i]=found[i]; i++;}
+    i=0; while (i<lim) {
+	var node=current[i++];
+	node.className=node.className.replace(/\bdisplayed\b/g,"").trim();}
+    var start=document.getElementById("abbyyleaf"+n);
+    var end=document.getElementById("abbyyleaf"+(n+1));
+    var textbox=document.getElementById("PAGETEXT");
+    while (start) {
+	if (start.parentNode===textbox) break;
+	else start=start.parentNode;}
+    while (end) {
+	if (end.parentNode===textbox) break;
+	else end=end.parentNode;}
+    var scan=start;
+    while ((scan)&&(scan!==end)) {
+	if (scan.nodeType===1)
+	    scan.className=scan.className+" displayed";
+	scan=scan.nextSibling;}
+    if ((scan)&&(scan.nodeType===1))
+	scan.className=scan.className+" displayed";
+    pageno=n;
+    var dispelt=document.getElementById("PAGEDISPLAY");
+    dispelt.innerHTML=(n).toString();
+    var inputelt=document.getElementById("PAGEINPUT");
+    inputelt.value=(n).toString();
+}
---
+++ beer/proto.scm
@@ -0,0 +1,36 @@
+(use-module '{fdweb xhtml texttools})
+(use-module '{openlibrary})
+
+(define abbyymakehtml (get-component "abbyymakehtml"))
+(define tmpdir "/tmp/")
+
+(define (showtext olib)
+  (let ((path (mkpath tmpdir (string-append olib ".html"))))
+    (forkwait abbyymakehtml path)
+    (xhtml (filestring path))))
+
+(define (padpageno num)
+  (stringout 
+    (if (< num 1000) "0")
+    (if (< num 100) "0")
+    (if (< num 10) "0")
+    num))
+
+(define (getbookpath olib)
+  (let* ((bookid (olib/get (olib/ref olib) 'ocaid))
+	 (metaurl (stringout "http://www.archive.org/download/"
+		    bookid "/" bookid "_meta.xml"))
+	 (location (get (urlget metaurl) 'location)))
+    (if (has-suffix location "_meta.xml")
+	(subseq location 0 (- 9))
+	location)))
+
+(define (getpageurl olib (pageno #f))
+  (let* ((bookid (olib/get (olib/ref olib) 'ocaid))
+	 (bookpath (getbookpath olib)))
+    (stringout "http://" (urihost bookpath)
+      "/BookReader/BookReaderImages.php?"
+      "zip=/" (printout (uripath bookpath) "_jp2.zip")
+      "&file=" (printout bookid "_jp2/" bookid "_"
+		 (if pageno (padpageno pageno) "%%%%")
+		 ".jp2&scale=4&rotate=0"))))
