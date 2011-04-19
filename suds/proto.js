667040ff030691ccc6674cd8a50d405bac8eda13
Ken Haase <kenh@archive.org>
2011-04-19 05:18:03 +0000

    Made offpage text be faded

--- suds/proto.js
+++ suds/proto.js
@@ -3,23 +3,24 @@ var pageno=false;
 
 function init()
 {
-    if (img_template) return img_template;
-    var meta_items=document.getElementsByTagName("meta");
-    var i=0; var lim=meta_items.length;
+  pagetext=document.getElementById("PAGETEXT");
+  if (img_template) return img_template;
+  var meta_items=document.getElementsByTagName("meta");
+  var i=0; var lim=meta_items.length;
+  while (i<lim) {
+    if (meta_items[i].name==="IMGTEMPLATE") {
+      img_template=meta_items[i].content;
+      break;}
+    else i++;}
+  var lineinfo=document.getElementsByClassName("abbyylineinfo");
+  if (lineinfo) {
+    var i=0; var lim=lineinfo.length;
     while (i<lim) {
-	if (meta_items[i].name==="IMGTEMPLATE") {
-	    img_template=meta_items[i].content;
-	    break;}
-	else i++;}
-    var lineinfo=document.getElementsByClassName("abbyylineinfo");
-    if (lineinfo) {
-	var i=0; var lim=lineinfo.length;
-	while (i<lim) {
-	    var info=lineinfo[i++];
-	    var newbreak=document.createElement("BR");
-	    newbreak.className="abbyybreak";
-	    info.parentNode.insertBefore(newbreak,info);}}
-    return img_template;
+      var info=lineinfo[i++];
+      var newbreak=document.createElement("BR");
+      newbreak.className="abbyybreak";
+      info.parentNode.insertBefore(newbreak,info);}}
+  return img_template;
 }
 
 function gotoPage(n)
@@ -37,8 +38,10 @@ function gotoPage(n)
     i=0; while (i<lim) {
 	var node=current[i++];
 	node.className=node.className.replace(/\bdisplayed\b/g,"").trim();}
-    var start=document.getElementById("abbyyleaf"+n);
-    var end=document.getElementById("abbyyleaf"+(n+1));
+    var leaf_start=document.getElementById("abbyyleaf"+n);
+    var start=leaf_start;
+    var leaf_end=document.getElementById("abbyyleaf"+(n+1));
+    var end=leaf_end;
     var textbox=document.getElementById("PAGETEXT");
     while (start) {
 	if (start.parentNode===textbox) break;
@@ -58,7 +61,70 @@ function gotoPage(n)
     dispelt.innerHTML=(n).toString();
     var inputelt=document.getElementById("PAGEINPUT");
     inputelt.value=(n).toString();
+    markoffpage(leaf_start,leaf_end);
 }
+
+function copy_array(array)
+{
+  var copy=[];
+  var i=0; var lim=array.length;
+  while (i<lim) copy.push(array[i++]);
+  return copy;
+}
+
+var pagetext;
+
+function markoffpage(start,end)
+{
+  var cleanup=copy_array(document.getElementsByClassName("offpage"));
+  var i=0; var lim=cleanup.length;
+  while (i<lim) {
+    var span=cleanup[i++];
+    var parent=span.parentNode;
+    var children=copy_array(span.childNodes);
+    var j=0; var jlim=children.length;
+    while (j<jlim) {
+      var child=children[j++];
+      parent.insertBefore(child,span);}
+    parent.removeChild(span);}
+  markbeforepage(start);
+  markafterpage(end);
+}
+
+function markbeforepage(start)
+{
+  if (start.parentNode===pagetext) return;
+  else {
+    var parent=start.parentNode;
+    var offpage=document.createElement("span");
+    offpage.className="offpage";
+    var children=copy_array(parent.childNodes);
+    var i=0; var lim=children.length;
+    while (i<lim)
+      if (children[i]===start) break;
+      else offpage.appendChild(children[i++]);
+    parent.insertBefore(offpage,start);
+    markbeforepage(parent);}
+}
+
+function markafterpage(end)
+{
+  if (end.parentNode===pagetext) return;
+  else {
+    var parent=end.parentNode;
+    var offpage=document.createElement("span");
+    offpage.className="offpage";
+    var children=copy_array(parent.childNodes);
+    var i=0; var lim=children.length; var hide=false;
+    while (i<lim) {
+      if (hide) offpage.appendChild(children[i]);
+      else if (children[i]===end) hide=true;
+      else {}
+      i++;}
+    parent.appendChild(offpage);
+    markafterpage(parent);}
+}
+
 
 function toggleClass(elt,classname)
 {
@@ -86,3 +152,4 @@ function leaf_keypress(evt)
 	evt.cancelBubble=true;
 	return false;}
 }
+
