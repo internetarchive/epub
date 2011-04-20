d3ee454d1fe691483cf010b5d69a36cbdee8839a
Ken Haase <kenh@archive.org>
2011-04-20 00:37:15 +0000

    Moved main php wrapper into proof.php, installed symbolic link

---
+++ suds/proof.php
@@ -0,0 +1,20 @@
+<?php
+
+$olib_arg=$_GET['OLIB'];
+$leaf_arg=$_GET['LEAF'];
+
+$contentType = 'text/html'; // default
+
+header('Content-type: ' . $contentType . ';charset=UTF-8');
+header('Access-Control-Allow-Origin: *'); // allow cross-origin requests
+
+if ((!($olib_arg))||(len($olib_arg))) $olib_arg="OL2588416M";
+if ((!($leaf_arg))||(len($leaf_arg))) $leaf_arg="42";
+					
+
+$olib = escapeshellarg($olib_arg);
+$leaf = escapeshellarg($leaf_arg);
+
+set_time_limit(120);
+passthru("python proof.py $olib $leaf 2>&1");
+?>
