7aa4357a40ea34d4144ac0a7769c37621ce1604a
Ken Haase <kenh@archive.org>
2011-04-27 00:22:51 +0000

    Moved form generation into epub module

---
+++ correctform.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+from urllib2 import urlopen, HTTPError
+from urlparse import urlparse
+
+import sys
+import subprocess
+import os
+import string
+import StringIO
+import json
+import cgi
+import cgitb
+import re
+import io
+
+cgitb.enable()
+
+import abbyyhtml
+import abbyygethtml
+
+abbyy2html=os.path.join(os.path.dirname(__file__),"../abbyy2html")
+tmp_cache=os.path.join(os.path.dirname(__file__),"../cache/%s.html")
+
+class BookHTML:
+    def __init__(self,body,style):
+        self.body=body
+        if (style):
+            self.style=style
+        else: self.style=""
+
+def getbookhtml(olib):
+    text=abbyygethtml.gethtml(olib)
+    style_start=re.search("(?i)<style[^>]*>",text)
+    style_end=re.search("(?i)</style[^>]*>",text)
+    body_start=re.search("(?i)<body[^>]*>",text)
+    body_end=re.search("(?i)</body[^>]*>",text)
+    body=False
+    style=False
+    if ((body_start) and (body_end)):
+        if ((style_start) and (style_end)):
+            style=text[style_start.end():style_end.start()]
+        body=text[body_start.end():body_end.start()]
+    else:
+        body=text
+    return BookHTML(body,style)
+
+def getbookpath(iaid):
+    tmp_url="http://www.archive.org/download/%s/%s_meta.xml"%(iaid,iaid)
+    urlstream=urlopen(tmp_url)
+    # read a little to get it to get the redirect
+    urlstream.read(3)
+    # Now get the actual URL
+    real_url=urlstream.geturl()
+    urlstream.close()
+    return real_url.replace("_meta.xml","")
+
+def getimageurl(iaid,bookpath):
+    parsed=urlparse(bookpath)
+    return (("http://"+parsed.netloc+"/BookReader/BookReaderImages.php")+
+            ("?zip="+parsed.path+"_jp2.zip")+
+            ("&file="+iaid+"_jp2/"+iaid+"_%%%%"+".jp2")+
+            ("&scale=4&rotate=0"))
+
+def getbooklink(olib):
+    return "http://www.openlibrary.org/books/"+olib
+
+def getauthorlink(olib):
+    return "http://www.openlibrary.org/authors/"+olib
+
+olib=sys.argv[1]
+if olib.startswith("/books/"):
+    olib=olib.slice(7)
+elif olib.startswith("books/"):
+    olib=olib.slice(6)
+leaf=int(sys.argv[2])
+
+olibstream=urlopen("http://www.openlibrary.org/books/%s.json"%olib)
+olibinfo=json.load(olibstream)
+iaid=olibinfo["ocaid"]
+if (not(iaid)):
+    error ("No archive reference for %s"%olib)
+authorid=olibinfo["authors"][0]["key"]
+olibstream=urlopen("http://www.openlibrary.org%s.json"%authorid)
+authorinfo=json.load(olibstream)
+
+bookpath=getbookpath(iaid)
+
+template=open(os.path.join(os.path.dirname(__file__),"suds/proof.html"),"rt",).read()
+html=getbookhtml(olib)
+rewrite=string.replace(
+    string.replace(
+        string.replace(
+            string.replace(
+                string.replace(
+                    string.replace(
+                        string.replace(
+                            string.replace(
+                                string.replace(template,"%%OLIB",olib),
+                                "%%LEAF",str(leaf)),
+                            "%%TITLE",olibinfo["title"]),
+                        "%%IMGTEMPLATE",getimageurl(iaid,bookpath)),
+                    "%%BOOKLINK",getbooklink(olib)),
+                "%%AUTHORLINK",getauthorlink(authorid)),
+            "%%AUTHORNAME",authorinfo["name"]),
+        "%%BOOKTEXT",html.body),
+    "%%STYLE",html.style)
+
+print rewrite.encode('utf-8')
+
+
--- suds/proof.php
+++ suds/proof.php
@@ -1,7 +1,7 @@
 <?php
 
-$olib_arg=$_GET['OLIB'];
-$leaf_arg=$_GET['LEAF'];
+$olib_arg=$_REQUEST['OLIB'];
+$leaf_arg=$_REQUEST['LEAF'];
 
 $contentType = 'text/html'; // default
 
@@ -10,11 +10,10 @@ header('Access-Control-Allow-Origin: *'); // allow cross-origin requests
 
 if ((!($olib_arg))||(strlen($olib_arg)==0)) $olib_arg="OL2588416M";
 if ((!($leaf_arg))||(strlen($leaf_arg)==0)) $leaf_arg="42";
-					
 
 $olib = escapeshellarg($olib_arg);
 $leaf = escapeshellarg($leaf_arg);
 
 set_time_limit(120);
-passthru("python proof.py $olib $leaf 2>&1");
+passthru("python ../correctform.py $olib $leaf 2>&1");
 ?>
